193e9d8251fc42baaa2b0b91edcce45142231d74
angelyactayosanchez <32417632+angelyactayosanchez@users.noreply.github.com>
2020-05-21 23:40:37 -0500

    Update User.kt

--- app/src/main/java/Models/User.kt
+++ app/src/main/java/Models/User.kt
@@ -3,7 +3,7 @@ package Models
 import android.widget.EditText
 
 class User( var id :String , var name : String,
-            var lastName : String, var email : String, var password : String) {
+            var lastName : String, var email : String, var password : String , var f : String) {
 //    private  var id : String
 //    private  var name : String
 //    private  var lastName : String
