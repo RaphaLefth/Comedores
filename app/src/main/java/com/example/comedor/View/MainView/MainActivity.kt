f95a49dd88a8bc2a42ea70f146701a6fe6727900
angel <1523353@utp.edu.pe>
2020-06-04 16:25:13 -0500

    last update mvp

--- app/src/main/java/com/example/comedor/View/MainView/MainActivity.kt
+++ app/src/main/java/com/example/comedor/View/MainView/MainActivity.kt
@@ -3,13 +3,37 @@ package com.example.comedor.View.MainView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.comedor.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var mDatabase : DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
 //		Thread.sleep(3500)
 //        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+
+        mDatabase.run {
+            child("User").child(mAuth!!.currentUser!!.uid).
+            addListenerForSingleValueEvent(object : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) {
+
+                }
+
+                override fun onDataChange(p0: DataSnapshot) {
+                    TODO("Not yet implemented")
+                }
+
+            })
+        }
     }
+
+
+
 }
