e1ce1136745572f45cf019c6d61c84e1d89776f9
angel <1523353@utp.edu.pe>
2020-06-06 12:02:00 -0500

    added principal presenter

--- app/src/main/java/com/example/comedor/View/MainView/MainActivity.kt
+++ app/src/main/java/com/example/comedor/View/MainView/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.comedor.View.MainView
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.comedor.Presenter.PrincipalPresenter.PrincipalPresenter
 import com.example.comedor.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.*
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var mAuth : FirebaseAuth
     private lateinit var mDatabase : DatabaseReference
+    private lateinit var presenter : PrincipalPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
 //		Thread.sleep(3500)
@@ -18,20 +20,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mAuth = FirebaseAuth.getInstance()
         mDatabase = FirebaseDatabase.getInstance().reference
+        presenter = PrincipalPresenter(mAuth,mDatabase,this)
+        presenter.msg()        
 
-        mDatabase.run {
-            child("User").child(mAuth!!.currentUser!!.uid).
-            addListenerForSingleValueEvent(object : ValueEventListener{
-                override fun onCancelled(p0: DatabaseError) {
 
-                }
-
-                override fun onDataChange(p0: DataSnapshot) {
-                    TODO("Not yet implemented")
-                }
-
-            })
-        }
+//        mDatabase.run {
+//            child("User").child(mAuth!!.currentUser!!.uid).
+//            addListenerForSingleValueEvent(object : ValueEventListener{
+//                override fun onCancelled(p0: DatabaseError) {
+//
+//                }
+//
+//                override fun onDataChange(p0: DataSnapshot) {
+//                }
+//
+//            })
+//        }
     }
 
 
